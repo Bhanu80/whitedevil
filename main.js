341d05ec1263df4484a8ba4fcbd158c05e60bb74
Bhanu80 <40848518+Bhanu80@users.noreply.github.com>
2018-07-07 11:55:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function getfile(file,callback){
+  var xhr = new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET",file,true);
+  xhr.onreadystatechange = function(){
+     if(xhr.readyState === 4 && xhr.status == "200"){
+       callback(xhr.responseText);
+
+    }
+  };
+  xhr.send(null);
+}
+ getfile("data.json",function(text){
+   var data = JSON.parse(text);
+   console.log(data);
+   career(data.career);
+
+})
+
+ var child2=document.querySelector(".childTwo");
+
+function career(careerInfo){
+  var careerHeading=document.createElement("h2");
+  child2.appendChild(careerHeading);
+var ch=document.createElement("p");
+ch.textContent=careerInfo.info;
+child2.appendChild(ch);
+}
